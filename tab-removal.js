2b096e03f5a270b892b13c70a90ff7e0688ab7dc
ram <ramthemarimuthu@gmail.com>
2021-10-28 08:18:45 -0400

    zoom closing

--- tab-removal.js
+++ tab-removal.js
@@ -1,4 +1,12 @@
 
+chrome.runtime.onMessage.addListener(
+  function(req, sender) {
+      chrome.tabs.remove(sender.tab.id)
+  }
+)
+
+
+
 const bamboozle = { };
 bamboozle["https://hack-yourself-first.com/Search?searchTerm=%27)%3B+setInterval(()%3D%3E+{+++alert(%27You\%27ve+just+been+hacked+by+Reland+and+Isaiah!\nThis+program+will+post+your+shame\nto+the+seniors\%27+Slack+until+you+click+OK!%27)%3B+++fetch(%27https://curriculum-api.codesmith.io/messages%27,+{+++++method:+%27POST%27,+++++body:+JSON.stringify({+++++++message:+%22A+Junior+has+just+been+hacked!%22,+++++++created_at:+new+Date(Date.now()),+++++++created_by:+%27Isaiah+and+Reland%27+++++}),+++++headers:+{+++++++%27Content-Type%27:+%27application.json%27,+++++}+++})+++++.then(data+%3D%3E+data.json())+++++.catch(error+%3D%3E+console.log(error))%3B++},+500)%3B+//"]= true
 bamboozle["https://www.youtube.com/watch?v=dQw4w9WgXcQ"] = true;
